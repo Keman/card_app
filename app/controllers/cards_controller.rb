4769d5f2b847496e0afd4aa51a889715181b7f6f
Keman <kemanorel23@gmail.com>
2015-07-10 17:42:45 +0500

    Fixed comments Vol.3

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,5 @@
 class CardsController < ApplicationController
-  before_action :find_card, :only => [:show, :edit, :update, :destroy]
+  before_action :find_card, only: [:show, :edit, :update, :destroy]
 
   def index
     @cards = Card.all
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,11 @@
 class Card < ActiveRecord::Base
-  before_validation :set_default_review_date, on: [ :create, :update ]
+  before_validation :set_default_review_date, on: :create
   validates :original_text, :translated_text, :review_date, presence: true
   validate :equality_check
 
   def equality_check
-    if (original_text.capitalize == translated_text.capitalize)
+    if (original_text.squish.mb_chars.capitalize ==
+        translated_text.squish.mb_chars.capitalize)
       errors.add(:original_text, "original and translated text can't be equal")
     end
   end
