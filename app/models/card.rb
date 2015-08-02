97490442b63dc83d168694cf63b4720c443bf956
Keman <kemanorel23@gmail.com>
2015-08-02 21:26:37 +0500

    =/

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,12 +3,11 @@ class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
   validate :equality_check
 
-  protected
+  private
     def set_default_review_date
       self.review_date = Time.now + 3.days
     end
 
-  private
     def equality_check
       if prepare_word(original_text) == prepare_word(translated_text)
         errors.add(:original_text, "original and translated text can't be equal")
