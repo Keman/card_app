2100b9cd87dd9e82e05760463b824fc3c17f29f1
Keman <kemanorel23@gmail.com>
2015-08-04 14:19:17 +0500

    Ooops

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-require 'open-uri'
+require "open-uri"
 
 url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 @doc = Nokogiri::HTML(open(url))
 
-def pull_date (fucking_variable)
-original_text = @doc.at_css("tr.row#{fucking_variable}:nth-child(#{@i}) > td:nth-child(2)").text
-translated_text = @doc.at_css("tr.row#{fucking_variable}:nth-child(#{@i}) > td:nth-child(3)").text
-Card.create(original_text: original_text, translated_text: translated_text)
+def pull_date(fucking_variable)
+  original_text = @doc.at_css("tr.row#{fucking_variable}:nth-child(#{@i}) > td:nth-child(2)").text
+  translated_text = @doc.at_css("tr.row#{fucking_variable}:nth-child(#{@i}) > td:nth-child(3)").text
+  Card.create(original_text: original_text, translated_text: translated_text)
 end
 
 def stupid_check
@@ -14,9 +14,9 @@ def stupid_check
 end
 
 for @i in 2..6
-    if stupid_check
-      pull_date ("B")
-    else
-      pull_date ("A")
-    end
+  if stupid_check
+    pull_date ("B")
+  else
+    pull_date ("A")
+  end
 end
