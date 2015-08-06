e7f7cca4b03e2c1c41de84e2c5e6a2b24f6dbadf
Keman <kemanorel23@gmail.com>
2015-08-07 03:09:04 +0500

    Fixed comments Vol.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,16 @@ require "open-uri"
 url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 @doc = Nokogiri::HTML(open(url))
 
-def pull_date(fucking_variable)
-  original_text = @doc.at_css("tr.row#{fucking_variable}:nth-child(#{@i}) > td:nth-child(2)").text
-  translated_text = @doc.at_css("tr.row#{fucking_variable}:nth-child(#{@i}) > td:nth-child(3)").text
+def pull_data(mystical_variable, i)
+  original_text = @doc.at_css("tr.row#{mystical_variable}:nth-child(#{i}) > td:nth-child(2)").text
+  translated_text = @doc.at_css("tr.row#{mystical_variable}:nth-child(#{i}) > td:nth-child(3)").text
   Card.create(original_text: original_text, translated_text: translated_text)
 end
 
-def stupid_check
-  @doc.at_css("tr.rowA:nth-child(#{@i}) > td:nth-child(2)").nil?
-end
-
-for @i in 2..6
-  if stupid_check
-    pull_date ("B")
+(2..6).each do |i|
+  if @doc.at_css("tr.rowA:nth-child(#{i}) > td:nth-child(2)").nil?
+    pull_data("B", i)
   else
-    pull_date ("A")
+    pull_data("A", i)
   end
 end
