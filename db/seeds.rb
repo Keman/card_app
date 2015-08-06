47add5e04b2d5edd14862f157b203cbe1c7118c3
Keman <kemanorel23@gmail.com>
2015-08-07 03:35:57 +0500

    row#{selector}

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ require "open-uri"
 url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 @doc = Nokogiri::HTML(open(url))
 
-def pull_data(mystical_variable, i)
-  original_text = @doc.at_css("tr.row#{mystical_variable}:nth-child(#{i}) > td:nth-child(2)").text
-  translated_text = @doc.at_css("tr.row#{mystical_variable}:nth-child(#{i}) > td:nth-child(3)").text
+def pull_data(selector, i)
+  original_text = @doc.at_css("tr.row#{selector}:nth-child(#{i}) > td:nth-child(2)").text
+  translated_text = @doc.at_css("tr.row#{selector}:nth-child(#{i}) > td:nth-child(3)").text
   Card.create(original_text: original_text, translated_text: translated_text)
 end
 
