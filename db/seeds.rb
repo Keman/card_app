c83f64608190b2818154684bc973a6305a5fc1b5
Keman <kemanorel23@gmail.com>
2015-08-11 19:14:42 +0500

    Nokogiri::XML::Element

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,12 @@
 require "open-uri"
 
 url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
-@doc = Nokogiri::HTML(open(url))
+doc = Nokogiri::HTML(open(url))
+nodes = doc.css("tr")
+nodes.shift
 
-def pull_data(selector, i)
-  original_text = @doc.at_css("tr.row#{selector}:nth-child(#{i}) > td:nth-child(2)").text
-  translated_text = @doc.at_css("tr.row#{selector}:nth-child(#{i}) > td:nth-child(3)").text
-  Card.create(original_text: original_text, translated_text: translated_text)
-end
-
-i = 2
-@doc.css("tbody:nth-child(#{i})").each do
-  if @doc.at_css("tr.rowA:nth-child(#{i}) > td:nth-child(2)").nil?
-    pull_data("B", i)
-    i += 1
-  else
-    pull_data("A", i)
-    i += 1
-  end
+nodes.each do |node|
+    original_text = node.first_element_child.next_element.inner_text
+    translated_text = node.last_element_child.previous_element.inner_text
+    Card.create(original_text: original_text, translated_text: translated_text)
 end
