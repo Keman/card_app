c964d8f3ef9a5c2b221c550826588242f514c350
Keman <kemanorel23@gmail.com>
2015-09-02 16:26:07 +0500

    Delete unnecessary method

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,9 +5,9 @@ class Card < ActiveRecord::Base
 
   scope :for_review, -> { where("review_date <= ?", Time.now).order ("random()") }
 
-  def check_translation(version)
-    if prepare_word(version) == prepare_word(translated_text)
-      set_review_date
+  def check_translation(version_of_translation)
+    if prepare_word(version_of_translation) == prepare_word(translated_text)
+      self.review_date = Time.now + 5.days
       save
       true
     else
@@ -19,10 +19,6 @@ class Card < ActiveRecord::Base
 
   def set_default_review_date
     self.review_date = Time.now + 3.days
-  end
-
-  def set_review_date
-    self.review_date = Time.now + 5.days
   end
 
   def equality_check
