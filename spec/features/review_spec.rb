0281122f121d88feb2f39086a5d142c2b07f4d86
Keman <kemanorel23@gmail.com>
2015-09-07 04:50:58 +0500

    Factorys and capybara

---
+++ spec/features/review_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe "review card process" do
+  it "should show right flash message" do
+    create(:card)
+    create(:card)
+
+    visit new_review_path
+    fill_in "review_version_of_translation", with: "Тест"
+    click_button "Проверить!"
+    expect(page).to have_content "Правильно :)"
+  end
+
+end
