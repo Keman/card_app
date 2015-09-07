94dd316b018a57136ab7f4d52bd3b70a35c63350
Keman <kemanorel23@gmail.com>
2015-09-07 21:52:33 +0500

    Renames the variable and removed placeholder

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,16 +2,16 @@ require "rails_helper"
 
 describe Card do
   context "#check_translation" do
-    let(:c) do
+    let(:card) do
       create(:card, original_text: "Test", translated_text: "Тест")
     end
 
     it "should correct verify compliance translations" do
-      expect(c.check_translation("тЕсТ")).to eq(true)
+      expect(card.check_translation("тЕсТ")).to eq(true)
     end
 
     it "should not pass incorrect translations" do
-      expect(c.check_translation("Текст")).to eq(false)
+      expect(card.check_translation("Текст")).to eq(false)
     end
   end
 
@@ -25,10 +25,10 @@ describe Card do
     end
 
     it "should set right default review date" do
-      u = User.create
-      c = Card.create(original_text: "Test", translated_text: "Тест", user_id: u.id)
+      user = User.create
+      card = Card.create(original_text: "Test", translated_text: "Тест", user_id: user.id)
 
-      expect(c.review_date).to eq(Time.now + 3.days)
+      expect(card.review_date).to eq(Time.now + 3.days)
     end
   end
 end
