365bd6483fe25d45091e3904bd2911a123d19e30
Keman <kemanorel23@gmail.com>
2015-10-27 03:16:31 +0500

    >_<

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, email_format: { message: "has invalid format" }
 
   def self.notify_pending_cards
-    User.where(id: Card.for_review.select(:user_id).group(:user_id)).find_each do |user|
+    User.where(id: Card.for_review.select(:user_id)).find_each do |user|
       NotificationsMailer.pending_cards(user).deliver_now
     end
   end
