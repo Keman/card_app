dc798637df693f1c7d07c3ac5854cd6ac8faa26a
Keman <kemanorel23@gmail.com>
2015-10-30 18:17:41 +0500

    I dont know how call this commit

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -4,9 +4,7 @@ class NotificationsMailer < ActionMailer::Base
   default from: "cardappmailer@gmail.com", template_path: "mailers/notifications"
 
   def pending_cards(user)
-    n = user.cards.for_review.count
-    @l = user.locale
-    @number_of_cards = @l = "ru" ? n.to_s + " " + Russian.p(n, "карточка", "карточки", "карточек") : pluralize(n, "card")
-    mail to: user.email, subject: I18n.t "notifications_mailer.pending_cards.subject"
+    @user = user
+    mail to: @user.email, subject: I18n.t "notifications_mailer.pending_cards.subject"
   end
 end
